d39bbe982280919622393c024ae4be7c1575c650
brent hartshorn <brenthartshorn@proton.me>
2025-09-01 11:55:48 -0700

    checking parity of each label group

--- spectre.py
+++ spectre.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import sys
 import numpy as np
 ## configlation
 #* increase this number for larger tilings.
@@ -359,3 +360,96 @@ def get_color_array(tile_transformation, label):
             print(f"Inalid color {rgb} {label}, {tile_transformation}")
     return COLOR_MAP[label]
 
+INFO = {'total':0, 'positive_x':0, 'positive_y':0, 'negative_x':0, 'negative_y':0, 'x_zeros':0, 'y_zeros':0, 'mystic':0}
+def reset_info():
+    for k in INFO: INFO[k]=0
+    INFO['others'] = {}
+
+def plotVertices(tile_transformation, label, scale=1.0):
+    vertices = (SPECTRE_POINTS if label != "Gamma2" else Mystic_SPECTRE_POINTS).dot(tile_transformation[:,:2].T) + tile_transformation[:,2]
+    ax = ay = 0.0
+    verts = []
+    for v in vertices:
+        x,y = v
+        x *= scale
+        y *= scale
+        verts.append((x,y))
+        ax += x
+        ay += y
+    ax /= len(vertices)
+    ay /= len(vertices)
+    rot,scl = trot_inv(tile_transformation)
+    if '--verbose' in sys.argv:
+        print('pos:', ax, ay)
+        print('rot:', rot)
+        print('scl:', scl)
+    INFO['total'] += 1
+    if label == "Gamma2":
+        INFO['mystic'] += 1
+    else:
+        if label not in INFO['others']: INFO['others'][label] = 0
+        INFO['others'][label] += 1
+
+    if ax < 0:
+        INFO['negative_x'] += 1
+    elif ax > 0:
+        INFO['positive_x'] += 1
+    else:
+        INFO['x_zeros'] += 1
+    if ay < 0:
+        INFO['negative_y'] += 1
+    elif ay > 0:
+        INFO['positive_y'] += 1
+    else:
+        INFO['y_zeros'] += 1
+
+def print_info():
+    print(INFO)
+    assert INFO['positive_x'] > INFO['negative_x']
+    assert INFO['negative_y'] > INFO['positive_y']
+    print('there are more right tiles than left tiles by:', INFO['positive_x']-INFO['negative_x'])
+    print('x left right ratio:', INFO['negative_x'] / INFO['positive_x'] )
+    print('x right left ratio:', INFO['positive_x'] / INFO['negative_x'] )
+
+    print('there are more bottom tiles than top tiles by:', INFO['negative_y']-INFO['positive_y'])
+    print('y bottom top ratio:', INFO['negative_y'] / INFO['positive_y'] )
+    print('y top bottom ratio:', INFO['positive_y'] / INFO['negative_y'] )
+
+    for label in INFO['others']:
+        v = INFO['others'][label]
+        if is_odd(v):
+            print('X',end='')
+        else:
+            print('O',end='')
+    print()
+
+def is_odd(v):
+    return v % 2
+
+def test(a=10.0, b=10.0, rotation=30, steps=(1,2,3,4,5,6)):
+    for iterations in steps:
+        x = buildSpectreTiles(iterations,a,b, rotation=rotation)
+        reset_info()
+        x["Delta"].forEachTile( plotVertices )
+        print('ITERATIONS:', iterations)
+        print_info()
+        if is_odd(iterations):
+            ## we assume this is always true, yet the other labels have dynamics
+            assert is_odd(INFO['mystic'])
+        ## the comment below is the output from 6 iterations of the others labels parity
+        #1 #OXOXXX     (iteration 1 is unique, not all labels are used)
+        #2 #OOXOOOOXX
+        #3 #OXOXXOXOO  (same as 5)
+        #4 #OOOOOOXXX  (same as 6)
+        #5 #OXOXXOXOO  (same as 3)
+        #6 #OOOOOOXXX  (same as 4)
+        ## because iteration 3 has the same pattern as iteration 5,
+        ## and iteration 4 has the same pattern as iteration 6,
+        ## we assume that this alternating will continue forever
+        ## this should be confirmed by a faster computer than can do more iterations.
+
+if __name__=='__main__':
+    if '--quick' in sys.argv:
+        test(steps=(1,2,3))
+    else:
+        test()
